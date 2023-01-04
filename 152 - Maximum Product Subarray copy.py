a51d23d70e0812c3b40942c955d5c3dc712e02dd
tz01x <abudr963rahman@gmail.com>
2023-01-04 23:48:30 +0600

    nc

--- 152 - Maximum Product Subarray copy.py
+++ 152 - Maximum Product Subarray copy.py
@@ -8,10 +8,12 @@ class Solution:
         for i in range(1,len(nums)):
             num = nums[i]
             values = (num, cmax*num, cmin*num)
-            cmax = max(*values)
-            cmin = min(*values)
-            res = max(cmax,res)
-        return res
+            
+            cmax = max(cmax,*values)
+            cmin = min(cmin,*values)
+            
+    
+        return cmax
 
 
 s = Solution()
